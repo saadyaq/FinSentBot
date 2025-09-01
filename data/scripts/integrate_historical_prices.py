7c097ff96d2639a9171ad1dd4138a631fedec44c
saadyaq <saadyaqine91@gmail.com>
2025-09-01 12:50:31 +0200

    Redoing the preparation of dataset

--- data/scripts/integrate_historical_prices.py
+++ data/scripts/integrate_historical_prices.py
@@ -4,3 +4,14 @@ import pandas as pd
 import json 
 import sys
 #Configuration
+BASE_DIR= Path(__file__).resolve().parent.parent
+RAW_DIR= BASE_DIR/"raw"
+Training_dir=BASE_DIR/"training_datasets"
+Training_dir.mkdir(parents=True,exist_ok=True)
+#Paramètres (similaires que prepare_dataset.py)
+
+OBSERVATION_WINDOW_MINUTES=10
+BUY_THRESHOLD=0.005
+SELL_THRESHOLD=-0.005
+
+class HistoricalPriceIntegrator:
